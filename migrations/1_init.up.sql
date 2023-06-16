ea5cc8fb18baeacef1c9338e1f17e112626c705f
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-16 16:14:17 +0330

    add migrate up sql file

---
+++ migrations/1_init.up.sql
@@ -0,0 +1,50 @@
+
+CREATE TABLE IF NOT EXISTS city (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255) UNIQUE NOT NULL ,
+    created_at datetime DEFAULT NOW(),
+    updated_at datetime DEFAULT NOW() ON UPDATE NOW()
+);
+
+CREATE TABLE IF NOT EXISTS airplane (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255) UNIQUE NOT NULL ,
+    created_at datetime DEFAULT NOW(),
+    updated_at datetime DEFAULT NOW() ON UPDATE NOW()
+);
+
+CREATE TABLE IF NOT EXISTS airline (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255) UNIQUE NOT NULL ,
+    created_at datetime DEFAULT NOW(),
+    updated_at datetime DEFAULT NOW() ON UPDATE NOW()
+);
+
+CREATE TABLE IF NOT EXISTS canceling_situation (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    description varchar(255) NOT NULL ,
+    data varchar(255) NOT NULL ,
+    created_at datetime DEFAULT NOW(),
+    updated_at datetime DEFAULT NOW() ON UPDATE NOW()
+);
+
+CREATE TABLE IF NOT EXISTS flight (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    dep_city_id int NOT NULL ,
+    arr_city_id int NOT NULL ,
+    dep_time datetime NOT NULL ,
+    arr_time datetime NOT NULL ,
+    airplane_id int NOT NULL ,
+    airline_id int NOT NULL ,
+    price int NOT NULL ,
+    cxl_sit_id int NOT NULL ,
+    left_seat int NOT NULL,
+    created_at datetime DEFAULT NOW(),
+    updated_at datetime DEFAULT NOW() ON UPDATE NOW(),
+
+    FOREIGN KEY (dep_city_id) REFERENCES city(id),
+    FOREIGN KEY (arr_city_id) REFERENCES city(id),
+    FOREIGN KEY (airplane_id) REFERENCES airplane(id),
+    FOREIGN KEY (airline_id) REFERENCES airline(id),
+    FOREIGN KEY (cxl_sit_id) REFERENCES canceling_situation(id)
+);
