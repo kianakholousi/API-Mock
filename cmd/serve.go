122ff7d783c06feaafc0618eebd590a20169fd17
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-16 17:03:13 +0330

    add serve command

---
+++ cmd/serve.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"flight-data-api/config"
+	"flight-data-api/database"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/spf13/cobra"
+)
+
+var serveConfigPath string
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start the server with the specified configuration",
+	Long: `Starts the server with the specified configuration.
+
+You must specify a custom configuration file in YAML format using the --config flag. By default, this command will not run without a configuration file.
+
+The server will listen for incoming requests on the port specified in the configuration file.
+
+It is recommended to run this command to start the server when it is ready to accept incoming traffic.
+	
+Usage:
+  mycommand serve --config [path]
+	
+Flags:
+  -c, --config string   Path to the YAML configuration file (required)
+  -h, --help            Help for serve`,
+	Run: func(cmd *cobra.Command, args []string) {
+		serve()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+	serveCmd.Flags().StringVarP(&serveConfigPath, "config", "c", "", "Path to the YAML configuration file (required)")
+	if err := serveCmd.MarkFlagRequired("config"); err != nil {
+		panic(err)
+	}
+}
+
+func serve() {
+	cfg, err := config.Init(config.Params{FilePath: serveConfigPath, FileType: "yaml"})
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = database.InitDB(cfg.Database) // should get db connection and use it in context
+	if err != nil {
+		panic(err)
+	}
+
+	e := echo.New()
+
+	if err := e.Start(fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port)); err != nil {
+		panic(err)
+	}
+}
--- database/mysql.go
+++ database/mysql.go
@@ -1,15 +1,16 @@
 package database
 
 import (
+	"flight-data-api/config"
 	"fmt"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func InitDB(username, password, host, name, dbname string, port int) (*gorm.DB, error) {
+func InitDB(cfg *config.Database) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
-		username, password, host, port, name)
+		cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.DB)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
