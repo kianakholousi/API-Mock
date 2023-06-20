2be8ddeacdd43c4555d35450c9c6d149036980d4
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-20 04:05:48 +0330

    add city model

---
+++ models/city.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+type City struct {
+	ID        int32     `gorm:"column:id;primaryKey;autoIncrement:true" json:"id"`
+	Name      string    `gorm:"column:name;not null" json:"name"`
+	CreatedAt time.Time `gorm:"column:created_at;default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at;default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
