b0eedec9ed41bc9fcfb61b2a25c94f16ef0d5472
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-25 15:02:01 +0330

    able flight.go to work

--- http/handler/flight.go
+++ http/handler/flight.go
@@ -1,8 +1,12 @@
 package handler
 
+import "C"
 import (
+	"flight-data-api/models"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
+	"net/http"
+	"time"
 )
 
 type Flight struct {
@@ -10,5 +14,27 @@ type Flight struct {
 }
 
 func (f *Flight) Get(c echo.Context) error {
-	return nil
+	arrCity := c.FormValue("arr_city")
+	depCity := c.FormValue("dep_city")
+	depTime, err := time.Parse("2006-01-02", c.FormValue("dep_time"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "Bad Request")
+	}
+
+	if arrCity == "" || depCity == "" {
+		return c.JSON(http.StatusBadRequest, "Bad Request")
+	}
+
+	var flights []models.Flight
+	err = f.DB.Debug().Joins("DepCity").Where("DepCity.name = ?", depCity).
+		Joins("ArrCity").Where("ArrCity.name = ?", arrCity).
+		Where("year(dep_time) = ?", depTime.Year()).
+		Where("month(dep_time) = ?", depTime.Month()).
+		Where("day(dep_time) = ?", depTime.Day()).
+		Find(&flights).Error
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSONPretty(http.StatusOK, flights, " ")
 }
