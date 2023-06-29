5d21df1c6b4d050f1925f3461f8ed0908ec225e0
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-29 18:30:18 +0330

    complete get airplanes handler

--- http/handler/airplanes.go
+++ http/handler/airplanes.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"flight-data-api/models"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
+	"net/http"
 )
 
 type Airplanes struct {
@@ -10,5 +12,16 @@ type Airplanes struct {
 }
 
 func (a *Airplanes) Get(ctx echo.Context) error {
-	return nil
+	var airplanes []models.Airplane
+	err := a.DB.Debug().Find(&airplanes).Error
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, "Internal Server Error")
+	}
+
+	response := make([]Airplane, 0, len(airplanes))
+	for _, val := range airplanes {
+		response = append(response, Airplane{ID: val.ID, Name: val.Name})
+	}
+
+	return ctx.JSON(http.StatusOK, response)
 }
