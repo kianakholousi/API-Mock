254e75bfbc8c6ded6b0219b61b7e4c03e6460619
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-29 18:20:24 +0330

    complete get cities handler

--- http/handler/cities.go
+++ http/handler/cities.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"flight-data-api/models"
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
+	"net/http"
 )
 
 type Cities struct {
@@ -12,5 +14,16 @@ type Cities struct {
 }
 
 func (c *Cities) Get(ctx echo.Context) error {
-	return nil
+	var cities []models.City
+	err := c.DB.Debug().Find(&cities).Error
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, "Internal Server Error")
+	}
+
+	response := make([]City, 0, len(cities))
+	for _, val := range cities {
+		response = append(response, City{ID: val.ID, Name: val.Name})
+	}
+
+	return ctx.JSON(http.StatusOK, response)
 }
