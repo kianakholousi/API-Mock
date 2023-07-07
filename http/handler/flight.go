da527739ff759c755468f40d945bbe198b297b50
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-07-07 14:10:09 +0330

    moving files to right place

--- http/handler/flight.go
+++ http/handler/flight.go
@@ -4,6 +4,7 @@ import (
 	"flight-data-api/models"
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
+	"sort"
 
 	"gorm.io/gorm"
 	"net/http"
@@ -78,5 +79,36 @@ func (f *Flight) Get(ctx echo.Context) error {
 		})
 	}
 
+	return ctx.JSONPretty(http.StatusOK, response, " ")
+}
+
+type FlightsDates struct {
+	DB *gorm.DB
+}
+
+type GetFlightsDatesResponse struct {
+	Dates []string
+}
+
+func (f *FlightsDates) Get(ctx echo.Context) error {
+	var dates []time.Time
+	err := f.DB.Debug().
+		Model(&models.Flight{}).
+		Distinct("DATE(dep_time)").
+		Scan(&dates).
+		Error
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, "Internal Server Error")
+	}
+
+	var response GetFlightsDatesResponse
+	response.Dates = make([]string, 0, len(dates))
+	for _, date := range dates {
+		response.Dates = append(response.Dates, date.Format("2006-01-02"))
+	}
+
+	sort.Slice(response.Dates, func(i, j int) bool {
+		return response.Dates[i] < response.Dates[j]
+	})
 	return ctx.JSONPretty(http.StatusOK, response, " ")
 }
